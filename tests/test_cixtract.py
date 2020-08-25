bc8204f5ca38833e9782170120791b1375045737
klaus <>
2020-08-25 18:34:47 +0900

    Refactoring test

--- tests/test_cixtract.py
+++ tests/test_cixtract.py
@@ -14,11 +14,18 @@ class CixtractTest(TestCase):
         self.assertEqual(res.text, result)
 
     def test_get_readme(self):
-        cixtract = Cixtract()
-
-        repo = "https://github.com/klau5dev/cixtract"
         with open("README.md") as f:
             result = f.read()
+        testcase = [
+            ("https://github.com/klau5dev/cixtract", result),
+            ("https://github.com/klau5dev/not_exist", "")
+            ]
+        cixtract = Cixtract()
+
+        for test in testcase:
+            res = cixtract.get_readme(test[0])
+            self.assertEqual(res, test[1])
+
 
         res = cixtract.get_readme(repo)
         self.assertEqual(res, result)
